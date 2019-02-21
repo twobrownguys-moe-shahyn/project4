7a0c9b4e5a9d5f29fabc071bfd07c6cc7f4a803a
Mohammed <moe.abbas@gmail.com>
2019-02-20 20:14:53 -0500

    fetching great success wawaweewa

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,33 @@
 /*jshint esversion: 6 */
 
-myApp = {};
+const myApp = {};
 
+myApp.callApi = (query) => {
+    const apiId = `a9b9b7a2`;
+    const apiKey = `5944b972a9f51cf045008ace83d83548`;
+    const endpoint = `http://api.yummly.com/v1/api/recipes?_app_id=${apiId}&_app_key=${apiKey}&q=${query}`;
+    fetch(endpoint)
+        .then(obj => obj.json())
+        .then(data => {
+            /* console.log(data.matches); */
+            const newArray = data.matches.map(item => {
+                return item;
+            });
+            newArray.forEach(obj => {
+                console.log(obj.sourceDisplayName);
+            });
 
-// vGet access to the Nutritionix API
+        });
 
+};
 
-
-myApp.getRecipes = function (queryString) {
-    return $.ajax({
-        url: `http://api.yummly.com/v1/api/recipes?_app_id=YOUR_ID&_app_key=YOUR_APP_KEY`,
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-Yummly-App-ID': 'a9b9b7a2',
-            'X-Yummly-App-Key': '5944b972a9f51cf045008ace83d83548'
-        },
-        data: {
-            requirePictures: true
-        },
-    })
-    myApp.recipeArray = [];
-}
-
-
+myApp.init = function () {
+    myApp.callApi('chicken');
+    console.log(query);
+};
 
 $(function () {
     myApp.init();
+
 });
 
-myApp.init = function () {
-    myApp.getRecipes();
-};
-
-console.log(myApp.getRecipes);
