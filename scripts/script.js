6c47810e6294673e40c89c4ed059ac1ee78a0ccf
Mohammed <moe.abbas@gmail.com>
2019-02-21 15:10:38 -0500

    updated index and js

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,29 @@
 /*jshint esversion: 6 */
-
 const myApp = {};
 
 myApp.callApi = (query) => {
     const apiId = `a9b9b7a2`;
     const apiKey = `5944b972a9f51cf045008ace83d83548`;
-    const endpoint = `http://api.yummly.com/v1/api/recipes?_app_id=${apiId}&_app_key=${apiKey}&q=${query}`;
+    const endpoint = `http://api.yummly.com/v1/api/recipes?_app_id=${apiId}&_app_key=${apiKey}&q=${query}&requirePictures=true`;
     fetch(endpoint)
         .then(obj => obj.json())
         .then(data => {
-            /* console.log(data.matches); */
-            const newArray = data.matches.map(item => {
-                return item;
-            });
-            newArray.forEach(obj => {
-                console.log(obj.sourceDisplayName);
-            });
+            const recipeArray = data.restaurants;
 
         });
-
 };
-
+myApp.getValue = (value) => {
+    $('.button').click(function (e) {
+        e.preventDefault();
+        value = $(this).val();
+        myApp.callApi(value);
+    });
+}
 myApp.init = function () {
-    myApp.callApi('chicken');
-    console.log(query);
+    myApp.callApi();
+    myApp.getValue();
+
+
 };
-
 $(function () {
-    myApp.init();
-
-});
-
+            myApp.init();
