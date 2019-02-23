72914b4ddaf6f0f754976e333cfe0c9e6abc7ad9
Mohammed <moe.abbas@gmail.com>
2019-02-23 18:28:54 -0500

    updated css and js

--- scripts/json.js
+++ scripts/json.js
@@ -6,16 +6,18 @@ const wines = {
             id: 1,
             price: '$',
             desc: 'Elegant earthy full bodied red wine. Great value for the the price.',
-            pairing: ['pork', 'red-wine-sauce', 'mushrooms'],
+            pairing: 'Pork',
             link: 'http://www.lcbo.com/lcbo/product/prelude-du-chateau-haut-pezat-2014/526947#.XG3Lq-hKjIU',
+            image: 'http://www.lcbo.com/content/dam/lcbo/products/526947.jpg/jcr:content/renditions/cq5dam.web.1280.1280.jpeg',
         },
         wineTwo = {
             name: 'Chateau Arnauld 2012',
             id: 2,
             price: '$$',
             desc: 'One of the true legends! A sommeliers favorite because of the complexity and long finish!',
-            pairing: ['steak', 'barbeque-sauce', 'chocolate'],
+            pairing: 'Beef',
             link: 'http://www.lcbo.com/lcbo/product/chateau-arnauld-2012/335918#.XG3PbuhKjIU',
+            image: 'http://www.lcbo.com/content/dam/lcbo/products/335918.jpg/jcr:content/renditions/cq5dam.web.1280.1280.jpeg',
 
         },
         wineThree = {
@@ -23,11 +25,10 @@ const wines = {
             id: 3,
             price: '$$$',
             desc: 'You are going to impress everybody with this wine! Haut Brion is one of the 5 great bordeaux wines every created! make sure to decant for an hour before drinking',
-            pairing: ['lamb', 'red-wine-sauce', 'yams'],
+            pairing: 'Lamb',
             link: 'http://www.lcbo.com/lcbo/product/chateau-haut-brion-2000/578716#.XG3P4-hKjIU',
-
+            image: 'http://www.lcbo.com/content/dam/lcbo/products/578716.jpg/jcr:content/renditions/cq5dam.web.1280.1280.jpeg',
         },
-
     ],
     whites: [
         wineOne = {
@@ -35,7 +36,7 @@ const wines = {
             id: 4,
             price: '$',
             desc: 'Chablis are always a true crowd pleaser! Louis Jadot is one of the oldest winemakers and a true safe bet! ',
-            pairing: ['fish', "vinaigrette", 'herbs'],
+            pairing: 'salmon',
             link: 'http://www.lcbo.com/lcbo/product/louis-jadot-chablis-2015/468751#.XG3RXOhKjIU',
         },
         wineTwo = {
@@ -43,19 +44,16 @@ const wines = {
             id: 5,
             price: '$$',
             desc: 'Sancerre is a royal wine. It used the be the favorite wine of french aristocrats!',
-            pairing: ['salad', 'vinaigrette', 'fruit'],
+            pairing: 'salad',
             link: 'http://www.lcbo.com/lcbo/product/domaine-bonnard-sancerre-2016/140525#.XG3SLehKjIU',
-
         },
         wineThree = {
             name: 'Stags Leap Winery Chardonnay 2016',
             id: 6,
             price: '$$$',
             desc: 'heavy dishes require heavy wines and nobody does it better than Stags leap! This wine is strong enough to accompany a heavy butter sauce',
-            pairing: ['chicken', 'butter-sauce', 'veggies'],
+            pairing: 'chicken',
             link: 'http://www.lcbo.com/lcbo/product/stags-leap-winery-chardonnay-2016/655381#.XG3Tp-hKjIU',
-
         },
-
     ]
 };
--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,152 @@
 /*jshint esversion: 6 */
+
 const myApp = {};
+const apiId = `a9b9b7a2`;
+const apiKey = `5944b972a9f51cf045008ace83d83548`;
+const endpoint = `http://api.yummly.com/v1/api/recipes?_app_id=${apiId}&${apiKey}`;
+
 
 myApp.callApi = (query) => {
-    const apiId = `a9b9b7a2`;
-    const apiKey = `5944b972a9f51cf045008ace83d83548`;
-    const endpoint = `http://api.yummly.com/v1/api/recipes?_app_id=${apiId}&_app_key=${apiKey}&q=${query}&requirePictures=true`;
-    fetch(endpoint)
-        .then(obj => obj.json())
-        .then(data => {
-            const recipeArray = data.restaurants;
-
-        });
+    $.ajax({
+        url: endpoint,
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Yummly-App-ID': apiId,
+            'X-Yummly-App-Key': apiKey,
+        },
+        data: {
+            q: query,
+            requirePictures: true,
+            maxResult: 12,
+            start: 1
+        }
+    }).then(data => {
+        myApp.displayRecipes(data);
+    });
 };
+
+myApp.callSecondApi = () => {
+    $('.recipeBox').click(function (e) {
+        id = $(this);
+        id = id[0].dataset.id;
+        e.preventDefault();
+        const getTheRecipe = `http://api.yummly.com/v1/api/recipe/${id}?_app_id=${apiId}&_app_key=${apiKey}`;
+        $.ajax({
+            url: getTheRecipe,
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Yummly-App-ID': apiId,
+                'X-Yummly-App-Key': apiKey,
+            }
+        }).then(info => {
+            console.log(info);
+            myApp.getFullRecipeHTML(info);
+        });
+    });
+};
+
 myApp.getValue = (value) => {
     $('.button').click(function (e) {
         e.preventDefault();
         value = $(this).val();
         myApp.callApi(value);
     });
-}
-myApp.init = function () {
+};
+
+myApp.displayRecipes = (data) => {
+    $('.recipes').empty();
+    data.matches.forEach(recipe => {
+        const recipeHTML = `<div data-id="${recipe.id}" class="recipeBox">
+                            <a>${recipe.recipeName}</a>
+                            <img src="${recipe.smallImageUrls[0]}">
+                            </div>`;
+                            $('.recipes').append(recipeHTML);
+        });
+    myApp.callSecondApi();
+};
+
+myApp.getFullRecipeHTML = (info) => {
+    $('.listItems').empty();
+    const recipeHTML =
+        `<div class="showRecipe">
+            <div class="showRecipeTitle>
+                <h2>${info.name}</h2>
+            </div>
+            <div class="recipeImg">
+                <img src="${info.images[0].hostedLargeUrl}">
+            </div>
+            <div class = "servings&time">
+                <p><span class='bold'>Number of Servings:</span> ${info.numberOfServings}</p>
+                <p><span class='bold'> Total Prep Time:</span> ${info.totalTime}</p> 
+             </div>
+        </div>
+            <div class="linkToRecipe">
+            <a href="${info.sourceRecipeUrl}"><button>Give me the recipe</button></a>
+        </div>
+        <div class="pairWithWine">
+            <button>Pair Me!</button>
+        </div>`;
+    $('.showRecipe').empty();
+    $('.showRecipe').prepend(recipeHTML);
+    const ingredientList = info.ingredientLines.map(foodItem => foodItem);
+    ingredientList.forEach(ingredient => $('.listIngredients').append(`<li>${ingredient}<li>`));
+
+    // const getFullRecipeButtons = 
+
+    // $('.link&pairButtons').append(getFullRecipeButtons);
+};
+
+myApp.displayWine = (info) => {
+    $('.pair').click(function (e) {
+        e.preventDefault();
+        const protein = info.name;
+        const redWines = wines.reds;
+        const filteredOption = redWines.filter((wine) => {
+            if (protein.toLowerCase().includes(wine.pairing.toLowerCase())) {
+                return true;
+            }
+            if (wine.pairing === protein) {
+                return true;
+            } else {
+                return false;
+            }
+        });
+        filteredOption.forEach(item => {
+            const wineHTML =
+                `<div class="wine">
+                    <h3>${item.name}</h3>
+                    <p>${item.desc}</p>
+                    <span>${item.price}</span>
+                    <div class="wineImg">
+                        <img src="${item.image}">
+                    </div>
+                    <a href="${item.link}">LCBO Page</a>
+                </div>`;
+            $('.showWine').empty();
+            $('.showWine').append(wineHTML);
+        });
+    });
+};
+
+myApp.getNextResults = () => {
+    $('.next').click(function (e) {
+        e.preventDefault();
+
+    });
+};
+
+myApp.init = () => {
     myApp.callApi();
     myApp.getValue();
+    myApp.getNextResults();
 
 
 };
+
+
 $(function () {
-            myApp.init();
+    myApp.init();
+
+});
